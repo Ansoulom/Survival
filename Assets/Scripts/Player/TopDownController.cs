9a3e845fc0714fc28eec3e7bdb34804574d75509
Anton Södergren <anton.sodergren@gmail.com>
2017-09-04 10:53:00 +0200

    Organized files and started on player controller.

---
+++ Assets/Scripts/Player/TopDownController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+namespace Player
+{
+    public class TopDownController : MonoBehaviour
+    {
+        private Vector2 _velocity;
+
+
+        // Use this for initialization
+        private void Start ()
+        {
+		    _velocity = Vector2.zero;
+        }
+	
+
+        // Update is called once per frame
+        private void Update ()
+        {
+		    Move();
+        }
+
+
+        private void Move()
+        {
+            transform.position += new Vector3(_velocity.x, _velocity.y, 0f);
+        }
+    }
+}
