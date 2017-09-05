bd30c12e897c7d2f0da7b65fead40d1caa209806
Anton Södergren <anton.sodergren@gmail.com>
2017-09-05 12:16:23 +0200

    First working character controller

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(TopDownController))]
+public class PlayerInput : MonoBehaviour
+{
+    private TopDownController _controller;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        _controller = GetComponent<TopDownController>();
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _controller.Input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+}
--- Assets/Scripts/Player/TopDownController.cs
+++ Assets/Scripts/Player/TopDownController.cs
@@ -1,30 +1,44 @@
 ﻿using UnityEngine;
 
-
-namespace Player
+[RequireComponent(typeof(Rigidbody))]
+public class TopDownController : MonoBehaviour
 {
-    public class TopDownController : MonoBehaviour
+    private Vector2 _input;
+
+    private Vector2 _velocity;
+    public float MaxSpeed = 5f;
+
+
+    public Vector2 Input
     {
-        private Vector2 _velocity;
+        get { return _input; }
+        set { _input = value.normalized; }
+    }
 
 
-        // Use this for initialization
-        private void Start ()
-        {
-		    _velocity = Vector2.zero;
-        }
-	
-
-        // Update is called once per frame
-        private void Update ()
-        {
-		    Move();
-        }
+    // Use this for initialization
+    private void Start()
+    {
+        _velocity = Vector2.zero;
+    }
 
 
-        private void Move()
-        {
-            transform.position += new Vector3(_velocity.x, _velocity.y, 0f);
-        }
+    // Update is called once per frame
+    private void Update()
+    {
+        _velocity = _input * MaxSpeed;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+        GetComponent<Rigidbody2D>().position = transform.position;
+    }
+
+
+    private void Move()
+    {
+        transform.position += new Vector3(_velocity.x, _velocity.y, 0f) * Time.deltaTime;
     }
 }
