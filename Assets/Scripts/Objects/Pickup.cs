811fb98dcb7adfe5125271d92875cb3b9a00793e
Anton Södergren <anton.sodergren@gmail.com>
2017-09-11 11:32:28 +0200

    Added material pickups.

---
+++ Assets/Scripts/Objects/Pickup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Pickup : MonoBehaviour
+{
+    public string Resource;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<Inventory>().AddResource(Resource);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Dictionary<string, int> Resources;
+    public List<Weapon> Weapons;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        Weapons = new List<Weapon>();
+        Resources = new Dictionary<string, int>();
+	}
+
+    public void AddResource(string resource, int count = 1)
+    {
+        if (!Resources.ContainsKey(resource))
+        {
+            Resources.Add(resource, count);
+        }
+        else
+        {
+            Resources[resource] += count;
+        }
+
+        DestroyObject(gameObject);
+    }
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+		
+	}
+}
