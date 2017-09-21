c697e8f57a89f03a64849c6e3b7800001c9b2a28
Anton Södergren <anton.sodergren@gmail.com>
2017-09-21 14:32:35 +0200

    Zombie waves!

---
+++ Assets/Scripts/UI/UIPrompt.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UIPrompt : MonoBehaviour
+{
+    public float PromptTime = 2;
+    public string Text = "";
+
+    private Timer _activeTimer;
+    private bool _active;
+
+	// Use this for initialization
+    private void Start ()
+    {
+        _activeTimer = new Timer(PromptTime);
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        if (_active && _activeTimer.Update(Time.deltaTime))
+        {
+            GetComponent<Text>().text = "";
+        }
+	}
+
+
+    public void Activate()
+    {
+        _active = true;
+        _activeTimer.Reset();
+        GetComponent<Text>().text = Text;
+    }
+}
