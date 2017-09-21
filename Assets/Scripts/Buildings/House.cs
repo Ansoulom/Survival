5e2318d774751b69a6d5c95ea4d166793ad5a987
Anton Södergren <anton.sodergren@gmail.com>
2017-09-21 14:09:13 +0200

    Player can now enter and exit houses, looks super derp!

--- Assets/Scripts/Buildings/House.cs
+++ Assets/Scripts/Buildings/House.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class House : MonoBehaviour, IBuilding
 {
+    public bool InRange;
+
+    public bool PlayerInside { get; private set; }
+    private Transform _player;
 
 	// Use this for initialization
 	void Start () {
@@ -12,12 +16,49 @@ public class House : MonoBehaviour, IBuilding
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update ()
+    {
+        if (InRange)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                if (PlayerInside)
+                {
+                    PlayerInside = false;
+                    SetHouseOpen(false);
+                }
+                else
+                {
+                    _player.position = transform.position;
+                    PlayerInside = true;
+                    SetHouseOpen(true);
+                }
+            }
+        }
 	}
 
-    public void Create()
+
+    private void SetHouseOpen(bool open)
     {
-        gameObject.SetActive(true);
+        GetComponent<BoxCollider2D>().enabled = !open;
+        transform.Find("Walls").gameObject.SetActive(open);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        InRange = true;
+        _player = other.transform;
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        InRange = false;
+        _player = null;
     }
 }
